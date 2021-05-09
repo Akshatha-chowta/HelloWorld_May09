2a891ceb73680295b46bede130beb994397342d3
akshatha <akshathachowta@gmail.com>
2021-05-09 05:07:10 +0000

    pushing printamessage class file to git

---
+++ src/test/PrintMmessage.java
@@ -0,0 +1,10 @@
+package test;
+
+public class PrintMmessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi welcome to programming class");
+	}
+
+}
